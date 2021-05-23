adc9104a1c01aa9f38bb84fe70e18a05235b236e
M. Gorshkov <mgorshkov@tle.one>
2021-05-23 21:33:16 +0300

    fixed CircularBlockBootstrapM

--- modified_arch_bootstrap.py
+++ modified_arch_bootstrap.py
@@ -7,17 +7,17 @@ class CircularBlockBootstrapM(CircularBlockBootstrap):
         self.set_samples(samples)
 
     def set_samples(self, samples):
-        assert samples <= self._num_items, "didn't fixed yet"
         self._samples = samples    
 
     def update_indices(self):
         num_blocks = self._samples // self.block_size
         if num_blocks * self.block_size < self._samples:
             num_blocks += 1
-        indices = self.random_state.randint(self._samples, size=num_blocks)
+
+        indices = self.random_state.randint(self._num_items, size=num_blocks)
         indices = indices[:, None] + np.arange(self.block_size)
         indices = indices.flatten()
-        indices %= self._samples
+        indices %= self._num_items
 
         if indices.shape[0] > self._samples:
             return indices[: self._samples]
