a9188e2030a63cdd79366a9bddf0e35a2e8c48df
M. Gorshkov <mgorshkov@tle.one>
2021-05-24 13:04:15 +0300

    new bootstrap methods

--- models.py
+++ models.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
+from modified_arch_bootstrap import *
+from arch.bootstrap import optimal_block_length
+
 
 def prices2returns(prices):
     return np.array([(prices[i+1] - prices[i]) / prices[i] for i in range(len(prices)-1)])
@@ -29,24 +32,24 @@ class BaseModel:
         self.date_format =      r"%Y-%m-%d"
         self.dates =            [datetime.datetime.strptime(date, self.date_format).date() for date in self.data['Date'][:]]
     
-    def simulate(self, steps, iterations, T=None, F=0):
-        self.pseudo_returns = np.zeros([iterations, steps])
+    def simulate(self, samples, iterations, T=None, F=0):
+        self.pseudo_returns = np.zeros([iterations, samples])
         
     def plot_last_simulation(self, N=10, padding=10, real_price=False, axis='dates'):
-        steps = self.pseudo_returns.shape[1]
-        if steps <= 0:  return
+        samples = self.pseudo_returns.shape[1]
+        if samples <= 0:  return
         p0 = self.prices[self.T-1]
         
         plt.figure(figsize=(10,5))
         plt.grid(True)
         
-        end = self.T+steps if real_price else self.T 
+        end = self.T+samples if real_price else self.T 
         if axis == 'dates': 
-            axis_simulated = self.dates[self.T-1:self.T+steps]
+            axis_simulated = self.dates[self.T-1:self.T+samples]
             axis_real = self.dates[self.T-padding:end]
         elif axis == 'numbers':
             axis_real = np.arange(self.T-padding, end)
-            axis_simulated = np.arange(self.T-1, self.T+steps)
+            axis_simulated = np.arange(self.T-1, self.T+samples)
             
         random_indices = np.random.choice(self.pseudo_returns.shape[0], N, replace=False)
         simulated_prices = returns2prices(p0, self.pseudo_returns[random_indices, :])
@@ -55,7 +58,7 @@ class BaseModel:
             sp = np.append(np.array([p0]), sp)
             plt.plot(axis_simulated, sp, color='green', linewidth=0.2)
             
-        print(self.dates[self.T-1], self.dates[self.T+steps], steps)
+        print(self.dates[self.T-1], self.dates[self.T+samples], samples)
             
         plt.plot(axis_real, self.prices[self.T-padding:end], color='blue', linewidth=2)
             
@@ -82,17 +85,17 @@ class BaseModel:
 
         return var, level
     
-    def evaluate(self, start, begin, end, evaluation_step, q, steps, iterations, plot_errors=False, window=False):
+    def evaluate(self, start, begin, end, evaluation_step, q, samples, iterations, plot_errors=False, window=False):
         if end == None:   end = len(self.prices)
         error = 0
         total = 0
         self.T = begin
         
-        for i in range(begin, end-max(steps, evaluation_step), evaluation_step):
-            self.simulate(steps, iterations, F=start, T=begin)
+        for i in range(begin, end-max(samples, evaluation_step), evaluation_step):
+            self.simulate(samples, iterations, F=start, T=begin)
             _, level = self.VaR(q)
 
-            if self.prices[self.T+steps] < level: 
+            if self.prices[self.T+samples] < level: 
                 error += 1
                 if plot_errors: self.plot_last_simulation(50, real_price=True)
 
@@ -100,19 +103,111 @@ class BaseModel:
             begin += evaluation_step
             if window: start += evaluation_step
 
-            
         return error/total
 
-class RandomSampling(BaseModel):
+class BHS(BaseModel):
+    """
+    Bootstrap Historical Simulation method
+    """
     def __init__(self, data):
         super().__init__(data)
     
-    def simulate(self, steps, iterations, F=0, T=None):
+    def simulate(self, samples, iterations, F=0, T=None):
         if T == None:   T = len(self.returns)
         self.T = T
 
         self.pseudo_returns = np.array([
-            np.random.choice(self.returns[F:self.T], steps) for _ in range(iterations)
+            np.random.choice(self.returns[F:self.T], samples) for _ in range(iterations)
         ])
         return self.pseudo_returns
-            
+
+class MonteCarlo(BaseModel):
+    """
+    Monte-Carlo based simulation method
+    """
+    def __init__(self, data):
+        super().__init__(data)
+    
+    def simulate(self, samples, iterations, F=0, T=None):
+        if T == None:   T = len(self.returns)
+        self.T = T
+
+        sigma = np.std(self.returns[F:self.T])
+        mu = np.mean(self.returns[F:self.T])
+
+        self.pseudo_returns = np.array([
+            sigma * np.random.randn(samples) + mu for _ in range(iterations)
+        ])
+        return self.pseudo_returns
+
+class CB_BHS(BaseModel):
+    """
+    Circular Block Bootstrap Historical Simulation method
+    """
+    def __init__(self, data, block_size=None):
+        super().__init__(data)
+        self.block_size = block_size
+    
+    def simulate(self, samples, iterations, F=0, T=None):
+        if T == None:   T = len(self.returns)
+        self.T = T
+
+        r = self.returns[F:self.T]
+
+        if not self.block_size: 
+            self.block_size = int(optimal_block_length(r)['circular'])
+
+        bs = CircularBlockBootstrapM(samples, self.block_size, r = r)
+
+        self.pseudo_returns = np.array([
+            bs['r'] for _, bs in bs.bootstrap(iterations)
+        ])
+
+        return self.pseudo_returns
+
+class S_BHS(BaseModel):
+    """
+    Stationary Bootstrap Historical Simulation method
+    """
+    def __init__(self, data, block_size=None):
+        super().__init__(data)
+        self.block_size = block_size
+    
+    def simulate(self, samples, iterations, F=0, T=None):
+        if T == None:   T = len(self.returns)
+        self.T = T
+        
+        r = self.returns[F:self.T]
+
+        if not self.block_size: 
+            self.block_size = int(optimal_block_length(r)['stationary'])
+
+        bs = StationaryBootstrapM(samples, self.block_size, r = r)
+
+        self.pseudo_returns = np.array([
+            bs['r'] for _, bs in bs.bootstrap(iterations)
+        ])
+
+        return self.pseudo_returns
+
+
+class MB_BHS(BaseModel):
+    """
+    Moving Block Bootstrap Historical Simulation method
+    """
+    def __init__(self, data, block_size):
+        super().__init__(data)
+        self.block_size = block_size
+    
+    def simulate(self, samples, iterations, F=0, T=None):
+        if T == None:   T = len(self.returns)
+        self.T = T
+
+        r = self.returns[F:self.T]
+        bs = MovingBlockBootstrapM(samples, self.block_size, r = r)
+
+        self.pseudo_returns = np.array([
+            bs['r'] for _, bs in bs.bootstrap(iterations)
+        ])
+
+        return self.pseudo_returns
--- modified_arch_bootstrap.py
+++ modified_arch_bootstrap.py
@@ -1,6 +1,15 @@
 from arch.bootstrap import StationaryBootstrap, IIDBootstrap, CircularBlockBootstrap, MovingBlockBootstrap, IndependentSamplesBootstrap
 import numpy as np
 
+try:
+    from arch.bootstrap._samplers import stationary_bootstrap_sample
+except ImportError:  # pragma: no cover
+    from arch.bootstrap._samplers_python import stationary_bootstrap_sample
+
+
+# Переопределены методы генерации семплов в StationaryBootstrap,
+# CircularBlockBootstrap, MovingBlockBootstrap
+
 class CircularBlockBootstrapM(CircularBlockBootstrap):
     def __init__(self, samples, block_size, *args, **kwargs):
         super().__init__(block_size, *args, **kwargs)
@@ -23,3 +32,43 @@ class CircularBlockBootstrapM(CircularBlockBootstrap):
             return indices[: self._samples]
         else:
             return indices
+
+class StationaryBootstrapM(StationaryBootstrap):
+    def __init__(self, samples, block_size, *args, **kwargs):
+        super().__init__(block_size, *args, **kwargs)
+        self.set_samples(samples)
+
+    def set_samples(self, samples):
+        self._samples = samples    
+
+    def update_indices(self):
+        indices = self.random_state.randint(self._num_items, size=self._samples)
+        indices = indices.astype(np.int64)
+        u = self.random_state.random_sample(self._num_items)
+
+        return stationary_bootstrap_sample(indices, u, self._p)
+
+
+class MovingBlockBootstrapM(MovingBlockBootstrap):
+    def __init__(self, samples, block_size, *args, **kwargs):
+        super().__init__(block_size, *args, **kwargs)
+        self.set_samples(samples)
+
+    def set_samples(self, samples):
+        self._samples = samples    
+
+    def update_indices(self):
+        num_blocks = self._samples // self.block_size
+        if num_blocks * self.block_size < self._samples:
+            num_blocks += 1
+
+        max_index = self._num_items - self.block_size + 1
+        indices = self.random_state.randint(max_index, size=num_blocks)
+        indices = indices[:, None] + np.arange(self.block_size)
+        indices = indices.flatten()
+
+        if indices.shape[0] > self._samples:
+            return indices[: self._samples]
+        else:
+            return indices
+
